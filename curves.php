2ad8bfa1fdcd11a3fc9224c119f2a85525fb12cb
Caleb Evans <calebevans.me@gmail.com>
2013-11-16 17:21:35 -0800

    Clarified the behavior of the animateLayerGroup() method
    - Also edited an example with Bezier curves

--- curves.php
+++ curves.php
@@ -42,13 +42,13 @@ $("canvas").drawQuadratic({
 $("canvas").drawBezier({
   strokeStyle: "#000",
   strokeWidth: 5,
-  x1: 50, y1: 50, // Start point
-  cx1: 200, cy1: 50, // Control point
-  cx2: 50, cy2: 150, // Control point
-  x2: 200, y2: 150, // Start/end point
-  cx3: 300, cy3: 150, // Control point
-  cx4: 150, cy4: 1, // Control point
-  x3: 350, y3: 50 // Start/end point
+  x1: 25, y1: 50, // Start point
+  cx1: 175, cy1: 50, // Control point
+  cx2: 25, cy2: 150, // Control point
+  x2: 175, y2: 150, // Start/end point
+  cx3: 275, cy3: 150, // Control point
+  cx4: 125, cy4: 1, // Control point
+  x3: 300, y3: 50 // Start/end point
 });
 </pre>
 </div>
